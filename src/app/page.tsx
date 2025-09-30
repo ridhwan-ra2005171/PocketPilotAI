33e3c857827dccf7527435f5366f424b54317f14
ridhwan-ra2005171 <ra2005171@qu.edu.qa>
2025-09-30 23:41:54 +0200

    homepage for guest
    conditional rendered for non users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import {currentUser} from '@clerk/nextjs/server'
+import Guest from '../../components/Guest'
+//this is the homepage, will be used for the landing page
+//async bc itll be in server side
+export default async function HomePage() {
+  const user = await currentUser();
+  if (!user) {
+    return <Guest />;
+  }
 
-function Homepage() {
   return (
     <div  className='text-red-400'> Homepage</div>
   )
 }
 
-export default Homepage
